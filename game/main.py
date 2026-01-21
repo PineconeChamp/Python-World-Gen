a10ecd57cc9de8a051d7684cc2fe20a9d753429e
= <=>
2026-01-21 10:34:04 +0000

    Added continents, infinite generation and scrolling

--- game/main.py
+++ game/main.py
@@ -1,9 +1,10 @@
 from game_logic.pygame_instance import PygameInstance
 from game_logic.middleware import add_tile_to_pygame
-from noise_logic.generate_map import CreateNoiseGrid, BinarizedNoise, ColouredMap
+from noise_logic.generate_map import CreateNoiseGrid, BinarizedNoise, ColouredMap, CreateHeightReference
 from matplotlib import pyplot as plt
 import numpy as np
 import pygame
+import time
 
 window_settings = {
     "width": 800,
@@ -13,26 +14,49 @@ window_settings = {
 game = PygameInstance(width=window_settings["width"], height=window_settings["height"])
 
 map_settings = {
-    "size": 25,
-    "scale": 0.05,
+    "size": 100,
+    "scale": 0.01,
     "octaves": 8,
     "persistence": 0.5,
-    "lacunarity": 2.0
+    "lacunarity": 2.0,
+    "base": 22,
+    "offset_x": 0,
+    "offset_y": 0
 }
 
-map_object = CreateNoiseGrid(map_settings["size"], 
-                      map_settings["size"],
-                      map_settings["scale"],
-                      map_settings["octaves"],
-                      map_settings["persistence"],
-                      map_settings["lacunarity"]
-                      )
+height_reference = CreateHeightReference(map_settings["size"], 
+                                map_settings["size"],
+                                0.02,
+                                map_settings["octaves"],
+                                map_settings["persistence"],
+                                map_settings["lacunarity"],
+                                map_settings["base"],
+                                map_settings["offset_x"],
+                                map_settings["offset_y"]
+                                )
 
-map = map_object.noise_grid
+min_and_max_height = height_reference.generate_height_reference()
 
-binarized_map = BinarizedNoise(map).apply()
+def pollmap():
 
-colored_map = ColouredMap(binarized_map).apply()
+    map_object = CreateNoiseGrid(map_settings["size"], 
+                                map_settings["size"],
+                                map_settings["scale"],
+                                map_settings["octaves"],
+                                map_settings["persistence"],
+                                map_settings["lacunarity"],
+                                map_settings["base"],
+                                map_settings["offset_x"],
+                                map_settings["offset_y"]
+                                )
+
+    map = map_object.noise_grid
+
+    binarized_map = BinarizedNoise(map, min_and_max_height[0], min_and_max_height[1]).apply()
+
+    colored_map = ColouredMap(binarized_map).apply()
+
+    return map, binarized_map, colored_map
 
 running = True
 
@@ -44,9 +68,16 @@ while running:
 
     game.clear_screen()
 
+    map, binarized_map, colored_map = pollmap()
+
+    time.sleep(0.01)
+
+    map_settings["offset_x"] += 0.01
+    map_settings["offset_y"] += 0.01
+
     start_x_pos = 100
     start_y_pos = 100
-    tile_size = 16
+    tile_size = 4
 
     offset_x = tile_size + tile_size/2
     offset_y = tile_size + tile_size/2
--- game/noise_logic/generate_map.py
+++ game/noise_logic/generate_map.py
@@ -1,10 +1,63 @@
 import noise
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 #scale = 0.05
 #octaves = 8
 #persistence = 0.5
 #lacunarity = 2.0
+
+class CreateHeightReference:
+    def __init__(self, width, height, scale, octaves, persistence, lacunarity, base=12, x_offset=0, y_offset=0):
+        self.width = width
+        self.height = height
+        self.scale = scale
+        self.octaves = octaves
+        self.persistence = persistence
+        self.lacunarity = lacunarity
+        self.base = base
+        self.x_offset = x_offset
+        self.y_offset = y_offset
+
+    def generate_height_reference(self):
+        height_ref = [
+        [noise.pnoise2(
+            x * self.scale + self.x_offset,
+            y * self.scale + self.y_offset,
+            octaves=self.octaves,
+            persistence=self.persistence,
+            lacunarity=self.lacunarity,
+            repeatx=1024, 
+            repeaty=1024,
+            base=self.base
+            )
+
+                for x in range(self.width)]
+            for y in range(self.height)
+        ]
+
+        flattened_list = [v for row in height_ref for v in row]
+        flattened_list.sort()
+
+        max_value = flattened_list[-1]
+        min_value = flattened_list[0]
+        if min_value <= 0:
+            min_value = 0.001
+
+        print(max_value, min_value)
+
+        world = np.array(height_ref)
+
+        plt.figure(figsize=(6, 6))
+        plt.imshow(world, cmap="gray")
+        plt.colorbar(label="Noise value")
+        plt.title("2D Perlin Noise")
+        plt.axis("off")
+        plt.show()
+
+        return [max_value, min_value]
+
 
 class CreateNoiseGrid:
 
@@ -38,30 +91,23 @@ class CreateNoiseGrid:
         return noise_grid
 
 class BinarizedNoise:
-    def __init__(self, noise_map):
+    def __init__(self, noise_map, maximum, minimum):
         self.noise_map = noise_map
+        self.min_value = minimum
+        self.max_value = maximum
 
     def apply(self):
-        flattened_list = [v for row in self.noise_map for v in row]
-        sortvalues = flattened_list.copy()
-        sortvalues.sort()
-
-        max_value = sortvalues[-1]
-        min_value = sortvalues[0]
-
-        print(max_value, min_value)
 
         map_max = 1.0
         map_min = 0.0
         
-        slope = (map_max - map_min) / (max_value - min_value)
+        slope = (map_max - map_min) / (self.max_value - self.min_value)
 
         for i in range(len(self.noise_map)):
             for j in range(len(self.noise_map[i])):
                 
-                mapped_value = map_min + slope * (self.noise_map[i][j] - min_value)
+                mapped_value = map_min + slope * (self.noise_map[i][j] - self.min_value)
                 self.noise_map[i][j] = round(mapped_value, 1)
-                print(round(mapped_value, 1))
                 
         return self.noise_map
 
@@ -94,7 +140,7 @@ class ColouredMap:
                     case 0.1:
                         self.binarized_map[i][j] = "#6685E9"
                     case 0:
-                        self.binarized_map[i][j] = "#2E489C"
+                        self.binarized_map[i][j] = "#526DC7"
                     case _:
                         self.binarized_map[i][j] = "#2E489C"
                     
